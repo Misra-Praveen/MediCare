674ed7dcffcdca2dc4297e5a5953f6bbcb1492a3
Misra-Praveen <vgipra@gmail.com>
2026-02-01 02:32:32 +0530

    add logic to fetch all category and sub-category

--- backend/src/routes/categoryRoute.ts
+++ backend/src/routes/categoryRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createCategory, createSubCategory } from "../controllers/CategoryController";
+import { createCategory, createSubCategory, getAllCategory, getAllSubCategory } from "../controllers/CategoryController";
 import protect from "../middleware/authMiddleware";
 import checkRole from "../middleware/checkRoleMiddleware";
 
@@ -7,5 +7,7 @@ const categoryRouter = Router();
 
 categoryRouter.post("/category", protect, checkRole("ADMIN"), createCategory)
 categoryRouter.post("/subCategory", protect, checkRole("ADMIN"), createSubCategory)
+categoryRouter.get("/category", protect, checkRole("ADMIN"), getAllCategory)
+categoryRouter.get("/subcategory", protect, checkRole("ADMIN"), getAllSubCategory)
 
 export default categoryRouter
