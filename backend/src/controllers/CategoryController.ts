c30f4bf76e99368d6bd7a93a6a652ae1bb659354
Misra-Praveen <vgipra@gmail.com>
2026-02-01 02:33:20 +0530

    add logic to fetch all category and sub-category

--- backend/src/controllers/CategoryController.ts
+++ backend/src/controllers/CategoryController.ts
@@ -35,3 +35,26 @@ export const createSubCategory = async (req: Request, res: Response)=>{
          return res.status(500).json({message: "SubCategory Creation Failed"})
     }
 }
+
+export const getAllCategory = async(req: Request, res:Response)=>{
+    try {
+        const category = await CategoryModel.find()
+        if(!category){
+            return res.status(404).json({message : "Category Not Found"})
+        }
+        return res.status(200).json({message: "fetch all category successfully",  category} )
+    } catch (error) {
+        return res.status(500).json({message: "failed to fetch all category"} )
+    }
+}
+export const getAllSubCategory = async(req: Request, res:Response)=>{
+    try {
+        const subcategory = await SubCategoryModel.find()
+        if(!subcategory){
+            return res.status(404).json({message : "Sub-Category Not Found"})
+        }
+        return res.status(200).json({message: "fetch all sub-category successfully",  subcategory} )
+    } catch (error) {
+        return res.status(500).json({message: "failed to fetch all sub-category"} )
+    }
+}
